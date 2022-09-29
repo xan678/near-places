8a844b0480bba9cb57e48dda5a6f4e2a315005b3
Ankush Ojha <alpha98689@gmail.com>
2022-09-29 10:59:50 +0530

    Initial client code

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { ToastContainer } from "react-toastify";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import "react-toastify/dist/ReactToastify.css"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+      <ToastContainer/>
+      <Routes>
+        <Route path='/' element = {<Home/>}></Route>
+        <Route path='/login' element = {<Login/>}></Route>
+        <Route path='/register' element = {<Register/>}></Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/redux/features/authSlice.js
@@ -0,0 +1,38 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import * as api from "../api";
+
+export const login = createAsyncThunk("auth/login", async({formValue, navigate, toast}) => {
+    try{
+        const response = await api.signIn(formValue);
+        toast.success("Login successfully");
+        navigate("/");
+        return response.data;
+    }catch(err){
+        console.log(err);
+    }
+});
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        user: null,
+        error: "",
+        loading: false
+    },
+    extraReducers: {
+        [login.pending]: (state, action) => {
+            state.loading = true;
+        },
+        [login.fulfilled]: (state, action) => {
+            state.loading = false;
+            localStorage.setItem("profile", JSON.stringify({...action.payload}));
+            state.user = action.payload;
+        },
+        [login.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload.message;
+        }
+    }
+});
+
+export default authSlice.reducer;
